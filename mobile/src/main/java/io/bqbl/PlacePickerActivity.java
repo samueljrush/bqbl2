e41a4cc29884ec7c705ccbd48a5893198b2d56ab
worlow <stevo8791@gmail.com>
2015-09-03 03:14:41 -0700

    Create PlacePickerActivity.java

---
+++ mobile/src/main/java/io/bqbl/PlacePickerActivity.java
@@ -0,0 +1,14 @@
+package io.bqbl;
+
+public class PlacePickerActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+  }
+
+  @Override
+  protected void onResume() {
+  }
+}
+
+
